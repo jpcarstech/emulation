1ae2a8e4de3305ec4566261612e3687cd933e8f9
jpcarstech <46807021+jpcarstech@users.noreply.github.com>
2021-05-19 21:09:52 -0400

    Update UpdateXeniaCanary.ps1
    Quick rewrite to use $env:temp directory. Actually shortens the script quite a bit. Will have to update this on the other scripts as well

--- UpdateXeniaCanary.ps1
+++ UpdateXeniaCanary.ps1
@@ -1,19 +1,15 @@
 #create folder
-New-Item -Path "c:\temp\" -Name "xenia_canary" -ItemType "directory"
+New-Item -Path "$env:temp" -Name "xenia_canary" -ItemType "directory"
 
 #Download Xenia Canary
-$out = 'c:\temp\xenia_canary\xenia_canary.zip'
-
+$out = "$env:temp\xenia_canary\xenia_canary.zip"
 wget "https://github.com/xenia-canary/xenia-canary/releases/latest/download/xenia_canary.zip" -OutFile $out
 
 #Extract Xenia_canary
-$sourcefile = "c:\temp\xenia_canary\xenia_canary.zip" Expand-Archive $sourcefile -DestinationPath 'c:\temp\xenia_canary\xenia_canary_latest\'
-
-#Copy/Overwrite what is in the emulator folder
-$sourceDirectory = "c:\temp\xenia_canary\xenia_canary_latest*" $destinationDirectory = "c:\emulatori\Xenia\xenia_canary" Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
+Expand-Archive $out -DestinationPath "D:\Emulators\Xbox 360\xenia_canary" -Force
 
 #Delete downloaded Xenia_canary files when done to prep for next version
-Remove-Item -Path "c:\temp\xenia_canary\" -recurse
+Remove-Item -Path "$env:temp\xenia_canary\" -recurse
 
 #Once Script is done, confirm success/close
 Read-Host -Prompt "Xenia_canary has updated! Press Enter to continue"
